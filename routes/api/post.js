9afcb95314bbde5d2cae35dd7e702808a84f4958
claudiocds1987 <cla8787@gmail.com>
2021-06-18 15:54:41 -0300

    upload y validacion de imagen con Multer

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,5 +1,56 @@
 const router = require("express").Router();
-// const multer = require("multer");
+// ******************* PARA HACER UPLOAD DE IMAGE ******************* //
+const multer = require("multer");
+const path = require('path');
+const uuid = require("uuid");
+const storage = multer.diskStorage({
+    destination: (req, file, callBack) => {
+      //carpeta donde va a guardar las imgs
+      callBack(null, "public/images");
+    },
+    filename: (req, file, callBack) => {
+      // uuid.v4() guarda la img con id aleatorio, para que no
+      // se pisen imagenes en el caso que se suban con el mismo nombre
+      callBack(
+        null,
+        uuid.v4() + path.extname(file.originalname).toLocaleLowerCase()
+      );
+    },
+  });
+  
+  const upload = multer({
+    storage,
+    dest: "images/",
+  
+    limits: { fileSize: 2000000 }, // max permitido de image, 2 mega byte de peso
+    fileFilter: (req, file, cb) => {
+      console.log('FILE: ' + JSON.stringify(file));
+      const fileTypes = /jpeg|jpg|png|gif/; // expresion regular
+      // mimetype checkea si el archivo es valido ej img/extension del archivo
+      const mimetype = fileTypes.test(file.mimetype);
+      // path.extname checkea si la img tiene la extension .jpeg o .jpg o .gif o .png
+      const extname = fileTypes.test(path.extname(file.originalname));
+      if (mimetype && extname) {
+        return cb(null, true);
+      } else {
+        cb("Error: el archivo debe ser una imagen valida");
+      }
+    },
+  }).single("image");
+  
+  router.route("/file").post(upload, (req, res, next) => {
+    // const file = (req).file;
+    const file = req.body.image;
+    console.log(file);
+    if (!file) {
+      console.log("No se subió ninguna imagen");
+    }
+    res.send(file);
+    var ruta = (req).file.path;
+    console.log("ruta de imagen: " + ruta);
+  });
+  
+  // ******************* FIN UPLOAD DE IMAGE ******************* //
 
 // model sequelized from db.js file
 const { Post } = require("../../db");
@@ -20,12 +71,33 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-// CREATE /api/posts
-router.post("/", async (req, res) => {
-  // res.send('Entra correctamente, funciona!');
-  const post = await Post.create(req.body);
-  res.json(post);
+router.post("/", upload, async (req, res) => {
+    // fs.renameSync(req.file.path, req.file.path + '.' + req.file.mimetype.split('/')[1]);
+    console.log('aca : ' + req.file.path);
+
+    const postObj = {
+      title: req.body.title,
+      content: req.body.content,
+      //image: req.file.originalname,
+      image: req.file.path,
+      categoryId: req.body.categoryId,
+      creationDate: req.body.creationDate,
+    }
+
+    //console.log(req.body);
+    // console.log('objeto: ' + JSON.stringify(postObj));
+
+    const post = await Post.create(postObj);
+    res.json(post);
 });
+
+
+// CREATE /api/posts
+// router.post("/", async (req, res) => {
+//   // res.send('Entra correctamente, funciona!');
+//   const post = await Post.create(req.body);
+//   res.json(post);
+// });
 
 // UPDATE WITH PATCH /api/posts/1
 router.patch("/:id", async (req, res) => {
