a200b841619efde8d92458ddde2ccf5b72173d2c
claudiocds1987 <cla8787@gmail.com>
2021-06-18 22:17:37 -0300

    Formateo de codigo

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const express = require('express');
+const express = require("express");
 // para variables de entorno npm i dotenv
-require('dotenv').config();
+require("dotenv").config();
 
-const path = require('path');
+const path = require("path");
 // import routes
-const apiRouter = require('./routes/api');
+const apiRouter = require("./routes/api");
 
 const app = express();
 
-// view Pug
-app.set('view engine', 'pug');
+// view Pug Template
+app.set("view engine", "pug");
 
-require('./db');
+require("./db");
 
 // MIDDLEWARES
 app.use(express.json());
-app.use(express.urlencoded({extended: false})) // estaba en true
+app.use(express.urlencoded({ extended: false })); // estaba en true
 // To save static files like images
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/api', apiRouter);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/api", apiRouter);
 // Ruta view pug
-app.get('/form', (req, res) => {
-    res.render('index');
-})
+app.get("/form", (req, res) => {
+  res.render("index");
+});
 
 // INICIAR CON nodemon index.js
 
 app.listen(3000, () => {
-    console.log('Servidor funcionando');
+  console.log("Servidor funcionando");
 });
