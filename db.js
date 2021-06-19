119ea26f7ba78604cbed2803f4d8ea9cceb819af
claudiocds1987 <cla8787@gmail.com>
2021-06-18 22:18:29 -0300

    Formateo de codigo

--- db.js
+++ db.js
@@ -1,24 +1,28 @@
 const Sequelize = require("sequelize");
-require('dotenv').config();
+require("dotenv").config();
 
 // import model
 const PostModel = require("./models/post");
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: "remotemysql.com",
-  dialect: "mysql",
-  define: {
-    timestamps: false
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "remotemysql.com",
+    dialect: "mysql",
+    define: {
+      timestamps: false,
+    },
   }
-});
+);
 
 // Model "Post" to interact with MySql db
 const Post = PostModel(sequelize, Sequelize);
 
-sequelize.sync({ force: false })
-.then(() => {
-    console.log('Tablas sincronizadas')
-})
+sequelize.sync({ force: false }).then(() => {
+  console.log("Tablas sincronizadas");
+});
 
 module.exports = {
   Post,
